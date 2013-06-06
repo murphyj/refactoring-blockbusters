a96db1d77b4c4530252df9ced7ab512a52e60089
james.murphy <jamurphy84@hotmail.com>
2013-06-06 22:19:27 +0100

    Second Refactor: Moved calculateRentalPrice over to Rental class Renamed method / variables to more appropriate naming conventions

--- src/main/java/uk/co/blockbusters/Customer.java
+++ src/main/java/uk/co/blockbusters/Customer.java
@@ -21,56 +21,35 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
+        double totalCost = 0;
         int frequentRenterPoints = 0;
         Iterator<Rental> rentals = this.rentals.iterator();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasNext()) {
-            double thisAmount = 0;
-            Rental each = (Rental) rentals.next();
+            double rentalCost = 0;
+            Rental rental = (Rental) rentals.next();
 
-            thisAmount = calculateRentalPrice(each);
+            rentalCost = rental.getCharge();
 
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                 each.getDaysRented() > 1) {
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                 rental.getDaysRented() > 1) {
                 frequentRenterPoints ++;
             }
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + rental.getMovie().getTitle() + "\t" +
+                    String.valueOf(rentalCost) + "\n";
+            totalCost += rentalCost;
         }
 
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(totalCost) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
         return result;
     }
 
-    private double calculateRentalPrice(Rental rental) {
-        double thisAmount = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2) {
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
-    }
 }
--- src/main/java/uk/co/blockbusters/Rental.java
+++ src/main/java/uk/co/blockbusters/Rental.java
@@ -17,4 +17,26 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    public double getCharge() {
+        double rentalPrice = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalPrice += 2;
+                if (getDaysRented() > 2) {
+                    rentalPrice += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                rentalPrice += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalPrice += 1.5;
+                if (getDaysRented() > 3) {
+                    rentalPrice += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return rentalPrice;
+    }
 }
