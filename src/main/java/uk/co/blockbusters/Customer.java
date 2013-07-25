3a3cb3a85721b91ccea94217da4a3b4b22159f61
James Murphy <jamurphy84@hotmail.com>
2013-07-25 17:45:34 +0100

    Second refactor - renamed parameter passed to amountFor function and removed unnecessary first parameter

--- src/main/java/uk/co/blockbusters/Customer.java
+++ src/main/java/uk/co/blockbusters/Customer.java
@@ -30,7 +30,7 @@ public class Customer {
             Rental each = (Rental) rentals.next();
 
             //determine amounts for each line
-            thisAmount = amountFor(thisAmount, each);
+            thisAmount = amountFor(each);
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -53,21 +53,22 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(double thisAmount, Rental each) {
-        switch (each.getMovie().getPriceCode()) {
+    private double amountFor(Rental rental) {
+        double thisAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (rental.getDaysRented() > 2) {
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (rental.getDaysRented() > 3) {
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
                 }
                 break;
         }
