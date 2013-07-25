4701161a9d4c25ada348bade0e6231b07420ed8c
James Murphy <jamurphy84@hotmail.com>
2013-07-25 17:55:31 +0100

    4th refactor - removed need to thisAmount variable inside statement() and replaced with call to function

--- src/main/java/uk/co/blockbusters/Customer.java
+++ src/main/java/uk/co/blockbusters/Customer.java
@@ -26,11 +26,7 @@ public class Customer {
         Iterator<Rental> rentals = this.rentals.iterator();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasNext()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.next();
-
-            //determine amounts for each line
-            thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -42,8 +38,8 @@ public class Customer {
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 
         // add footer lines
